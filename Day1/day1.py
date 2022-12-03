a7c102933f352bad272d272ee8dd7fd4504efeb5
kPinney <kevinpinney@gmail.com>
2022-12-03 11:01:08 -0600

    one

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,5 +1,5 @@
 mycount = 0
-with open('day1input.txt', 'r') as f:
+with open('Day1/day1input.txt', 'r') as f:
 ##    print(line.strip())
     count = 0
     for line in f:
@@ -7,9 +7,4 @@ with open('day1input.txt', 'r') as f:
             count += 1
 print(count)
 
-##with open('day1input.txt', 'r') as f:
-##        print(' '.join(f.splitlines()))
-with open('day1input.txt', 'r') as f:
-    for line in f:
-        if line != '\n':
-            print([int(x) for x in line])
+##            print([int(x) for x in line])
